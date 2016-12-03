8c6ed50f9c617b8773c61b6ff6d30e647def7c9a
robertsrosa15 <robertsrosa15@gmail.com>
2016-12-03 00:21:52 -0500

    The camera modular, it has some errors

--- app/src/main/java/com/rsquared/robert/emotifaceit/FullscreenActivity.java
+++ app/src/main/java/com/rsquared/robert/emotifaceit/FullscreenActivity.java
@@ -1,18 +1,27 @@
 package com.rsquared.robert.emotifaceit;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Camera;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+import java.io.IOException;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
+public class FullscreenActivity extends AppCompatActivity implements {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -160,4 +169,111 @@ public class FullscreenActivity extends AppCompatActivity {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
+
+
+
+//    public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
+//
+//        public CameraView(Context context, Camera camera) {
+//            super(context);
+//        }
+//
+//        @Override
+//        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+//
+//        }
+//
+//        @Override
+//        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+//
+//        }
+//
+//        @Override
+//        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+//
+//        }
+//
+//        private SurfaceHolder mHolder;
+//        private Camera mCamera;
+//
+//        public FullscreenActivity(Context context, Camera camera) {
+//            super(context);
+//
+//            mCamera = camera;
+//            mCamera.setDisplayOrientation(90);
+//            //get the holder and set this class as the callback, so we can get camera data here
+//            mHolder = getHolder();
+//            mHolder.addCallback(this);
+//            mHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
+//        }
+//
+//        @Override
+//        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+//            try {
+//                //when the surface is created, we can set the camera to draw images in this surfaceholder
+//                mCamera.setPreviewDisplay(surfaceHolder);
+//                mCamera.startPreview();
+//            } catch (IOException e) {
+//                Log.d("ERROR", "Camera error on surfaceCreated " + e.getMessage());
+//            }
+//        }
+//
+//        @Override
+//        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+//            //before changing the application orientation, you need to stop the preview, rotate and then start it again
+//            if (mHolder.getSurface() == null)//check if the surface is ready to receive camera data
+//                return;
+//
+//            try {
+//                mCamera.stopPreview();
+//            } catch (Exception e) {
+//                //this will happen when you are trying the camera if it's not running
+//            }
+//
+//            //now, recreate the camera preview
+//            try {
+//                mCamera.setPreviewDisplay(mHolder);
+//                mCamera.startPreview();
+//            } catch (IOException e) {
+//                Log.d("ERROR", "Camera error on surfaceChanged " + e.getMessage());
+//            }
+//        }
+//
+//        @Override
+//        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+//            //our app has only one screen, so we'll destroy the camera in the surface
+//            //if you are unsing with more screens, please move this code your activity
+//            mCamera.stopPreview();
+//            mCamera.release();
+//        }
+//
+//        private Camera mCamera = null;
+//        private CameraView mCameraView = null;
+//
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_fullscreen);
+//
+//            try {
+//                mCamera = Camera.open();//you can use open(int) to use different cameras
+//            } catch (Exception e) {
+//                Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+//            }
+//
+//            if (mCamera != null) {
+//                mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+//                FrameLayout camera_view = (FrameLayout) findViewById(R.id.camera_view);
+//                camera_view.addView(mCameraView);//add the SurfaceView to the layout
+//            }
+//
+//            //btn to close the application
+//            ImageButton imgClose = (ImageButton) findViewById(R.id.imgClose);
+//            imgClose.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    System.exit(0);
+//                }
+//            });
+//        }
+//    }
 }
