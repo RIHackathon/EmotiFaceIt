2e5b37eac39106b9d3addc71a1cdee1b2de98ff7
robertsrosa15@gmail.com <robertsrosa15@gmail.com>
2016-12-03 12:25:24 -0500

    Added functionality to save bitmap

--- app/src/main/java/com/rsquared/robert/emotifaceit/ImageAdapter.java
+++ app/src/main/java/com/rsquared/robert/emotifaceit/ImageAdapter.java
@@ -1,11 +1,16 @@
 package com.rsquared.robert.emotifaceit;
 
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Bitmap;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by R^2 on 12/3/2016.
@@ -26,7 +31,7 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return mThumbIds.length;
+        return 1;
     }
 
     @Override
@@ -42,12 +47,35 @@ public class ImageAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View view, ViewGroup viewGroup) {
         ImageView imageView = new ImageView(context);
-        imageView.setImageResource(mThumbIds[position]);
+//        imageView.setImageResource(mThumbIds[position]);
         imageView.setImageBitmap(bitmap);
         return imageView;
     }
 
-    private Integer[] mThumbIds ={
+    private String saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(context.getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,"profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
+    }
+/*    private Integer[] mThumbIds ={
             R.drawable.ic_bus_stop_big_medium_small_turk,
             R.drawable.ic_bus_stop_big_medium_small_turk,
             R.drawable.ic_bus_stop_big_medium_small_turk,
@@ -65,5 +93,5 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.ic_bus_stop_big_medium_small_turk,
             R.drawable.ic_bus_stop_big_medium_small_turk
 
-    };
+    };*/
 }
