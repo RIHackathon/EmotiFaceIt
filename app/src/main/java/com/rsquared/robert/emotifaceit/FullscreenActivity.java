d03eacce298302580dcefc498a1097038a215487
robertsrosa15 <robertsrosa15@gmail.com>
2016-12-03 17:07:14 -0500

    The camera does not has the alert anymore

--- app/src/main/java/com/rsquared/robert/emotifaceit/FullscreenActivity.java
+++ app/src/main/java/com/rsquared/robert/emotifaceit/FullscreenActivity.java
@@ -7,9 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
 import android.net.Uri;
@@ -25,12 +23,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.io.ByteArrayOutputStream;
 
-import static android.R.attr.bitmap;
 
 
 public class FullscreenActivity extends AppCompatActivity{
@@ -51,7 +47,7 @@ public class FullscreenActivity extends AppCompatActivity{
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             if (getFromPref(this, ALLOW_KEY)) {
-                showSettingsAlert();
+//                showSettingsAlert();
             } else if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.CAMERA)
 
@@ -60,7 +56,7 @@ public class FullscreenActivity extends AppCompatActivity{
                 // Should we show an explanation?
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                         Manifest.permission.CAMERA)) {
-                    showAlert();
+//                    showAlert();
                 } else {
                     // No explanation needed, we can request the permission.
                     ActivityCompat.requestPermissions(this,
@@ -106,7 +102,7 @@ public class FullscreenActivity extends AppCompatActivity{
 
         ArrayAdapter<Drawable> stringArrayAdapter = new ArrayAdapter<Drawable>(this, android.R.layout.simple_list_item_1);
         listView.setAdapter(stringArrayAdapter);
-        showAlert();
+//        showAlert();
         stringArrayAdapter.add(myIcon);
         AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
             @Override
@@ -133,90 +129,90 @@ public class FullscreenActivity extends AppCompatActivity{
         return (myPrefs.getBoolean(key, false));
     }
 
-    private void showAlert() {
-        AlertDialog alertDialog = new AlertDialog.Builder(FullscreenActivity.this).create();
-        alertDialog.setTitle("Alert");
-        alertDialog.setMessage("App needs to access the Camera.");
+//    private void showAlert() {
+//        AlertDialog alertDialog = new AlertDialog.Builder(FullscreenActivity.this).create();
+//        alertDialog.setTitle("Alert");
+//        alertDialog.setMessage("App needs to access the Camera.");
+//
+//        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                        finish();
+//                    }
+//                });
+//
+//        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "ALLOW",
+//                new DialogInterface.OnClickListener() {
+//
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                        ActivityCompat.requestPermissions(FullscreenActivity.this,
+//                                new String[]{Manifest.permission.CAMERA},
+//                                MY_PERMISSIONS_REQUEST_CAMERA);
+//                    }
+//                });
+//        alertDialog.show();
+//    }
 
-        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        finish();
-                    }
-                });
+//    private void showSettingsAlert() {
+//        AlertDialog alertDialog = new AlertDialog.Builder(FullscreenActivity.this).create();
+//        alertDialog.setTitle("Alert");
+//        alertDialog.setMessage("App needs to access the Camera.");
+//
+//        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
+//                new DialogInterface.OnClickListener() {
+//
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                        //finish();
+//                    }
+//                });
 
-        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "ALLOW",
-                new DialogInterface.OnClickListener() {
+//        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "SETTINGS",
+//                new DialogInterface.OnClickListener() {
+//
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                        startInstalledAppDetailsActivity(FullscreenActivity.this);
+//                    }
+//                });
+//
+//        alertDialog.show();
+//    }
 
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        ActivityCompat.requestPermissions(FullscreenActivity.this,
-                                new String[]{Manifest.permission.CAMERA},
-                                MY_PERMISSIONS_REQUEST_CAMERA);
-                    }
-                });
-        alertDialog.show();
-    }
-
-    private void showSettingsAlert() {
-        AlertDialog alertDialog = new AlertDialog.Builder(FullscreenActivity.this).create();
-        alertDialog.setTitle("Alert");
-        alertDialog.setMessage("App needs to access the Camera.");
-
-        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "DONT ALLOW",
-                new DialogInterface.OnClickListener() {
-
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        //finish();
-                    }
-                });
-
-        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "SETTINGS",
-                new DialogInterface.OnClickListener() {
-
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        startInstalledAppDetailsActivity(FullscreenActivity.this);
-                    }
-                });
-
-        alertDialog.show();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA: {
-                for (int i = 0, len = permissions.length; i < len; i++) {
-                    String permission = permissions[i];
-
-                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
-                        boolean
-                                showRationale =
-                                ActivityCompat.shouldShowRequestPermissionRationale(
-                                        this, permission);
-
-                        if (showRationale) {
-                            showAlert();
-                        } else if (!showRationale) {
-                            // user denied flagging NEVER ASK AGAIN
-                            // you can either enable some fall back,
-                            // disable features of your app
-                            // or open another dialog explaining
-                            // again the permission and directing to
-                            // the app setting
-                            saveToPreferences(FullscreenActivity.this, ALLOW_KEY, true);
-                        }
-                    }
-                }
-            }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+//        switch (requestCode) {
+//            case MY_PERMISSIONS_REQUEST_CAMERA: {
+//                for (int i = 0, len = permissions.length; i < len; i++) {
+//                    String permission = permissions[i];
+//
+//                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+//                        boolean
+//                                showRationale =
+//                                ActivityCompat.shouldShowRequestPermissionRationale(
+//                                        this, permission);
+//
+//                        if (showRationale) {
+//                            showAlert();
+//                        } else if (!showRationale) {
+//                            // user denied flagging NEVER ASK AGAIN
+//                            // you can either enable some fall back,
+//                            // disable features of your app
+//                            // or open another dialog explaining
+//                            // again the permission and directing to
+//                            // the app setting
+//                            saveToPreferences(FullscreenActivity.this, ALLOW_KEY, true);
+//                        }
+//                    }
+//                }
+//            }
 
             // other 'case' lines to check for other
             // permissions this app might request
-        }
-    }
+//        }
+//    }
 
     @Override
     protected void onResume() {
