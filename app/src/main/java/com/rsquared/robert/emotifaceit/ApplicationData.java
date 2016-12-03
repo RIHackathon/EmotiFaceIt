a5e556afeb88d1b37d4b40e81c9f31ecf2b4ec75
suhas <avsuhas@gmail.com>
2016-12-03 14:37:29 -0500

    avsuhas

---
+++ app/src/main/java/com/rsquared/robert/emotifaceit/ApplicationData.java
@@ -0,0 +1,19 @@
+package com.rsquared.robert.emotifaceit;
+
+/**
+ * Created by suhasarudi on 12/3/16.
+ */
+public class ApplicationData {
+
+    public static String CLIENT_ID ="a7109df18b4943959dfad0cd4b0d9409";
+    public static String CLIENT_SECRET ="e267684c4b18494b9de7d2c8c4173fb0";
+    public static String CALLBACK_URL ="https://github.com/RIHackathon/EmotiFaceIt";
+
+    /**
+     * Created by suhasarudi on 12/3/16.
+     */
+
+
+
+}
+
