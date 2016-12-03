14e422e17881d401b369167b023e47ec0de55dc2
robertsrosa15 <robertsrosa15@gmail.com>
2016-12-03 02:03:59 -0500

    The code does not has any error but still giving a black screen

--- app/src/main/java/com/rsquared/robert/emotifaceit/CameraView.java
+++ app/src/main/java/com/rsquared/robert/emotifaceit/CameraView.java
@@ -1,49 +1,22 @@
 package com.rsquared.robert.emotifaceit;
 
 import android.content.Context;
-import android.graphics.Camera;
-import android.os.Bundle;
+import android.hardware.Camera;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
-
-import com.rsquared.robert.emotifaceit.R;
 
 import java.io.IOException;
 
-public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
-
-    public CameraView(Context context, Camera camera, Camera mCamera) {
-        super(context);
-        this.mCamera = mCamera;
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
-    }
-
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
     private SurfaceHolder mHolder;
     private Camera mCamera;
 
-    public CameraView(CameraView context, Camera camera) {
+    public CameraView(Context context, Camera camera){
         super(context);
 
         mCamera = camera;
-        mCamera.setDisplayOrentation(90);
+        mCamera.setDisplayOrientation(90);
         //get the holder and set this class as the callback, so we can get camera data here
         mHolder = getHolder();
         mHolder.addCallback(this);
@@ -52,7 +25,7 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        try {
+        try{
             //when the surface is created, we can set the camera to draw images in this surfaceholder
             mCamera.setPreviewDisplay(surfaceHolder);
             mCamera.startPreview();
@@ -64,17 +37,17 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
         //before changing the application orientation, you need to stop the preview, rotate and then start it again
-        if (mHolder.getSurface() == null)//check if the surface is ready to receive camera data
+        if(mHolder.getSurface() == null)//check if the surface is ready to receive camera data
             return;
 
-        try {
+        try{
             mCamera.stopPreview();
-        } catch (Exception e) {
+        } catch (Exception e){
             //this will happen when you are trying the camera if it's not running
         }
 
         //now, recreate the camera preview
-        try {
+        try{
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
         } catch (IOException e) {
@@ -88,34 +61,5 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
         //if you are unsing with more screens, please move this code your activity
         mCamera.stopPreview();
         mCamera.release();
-    }
-
-    private Camera mCamera = null;
-    private CameraView mCameraView = null;
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fullscreen);
-
-        try {
-            mCamera = Camera.open();//you can use open(int) to use different cameras
-        } catch (Exception e) {
-            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
-        }
-
-        if (mCamera != null) {
-            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
-            FrameLayout camera_view = (FrameLayout) findViewById(R.id.camera_view);
-            camera_view.addView(mCameraView);//add the SurfaceView to the layout
-        }
-
-        //btn to close the application
-        ImageButton imgClose = (ImageButton) findViewById(R.id.imgClose);
-        imgClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.exit(0);
-            }
-        });
     }
 }
--- app/src/main/java/com/rsquared/robert/emotifaceit/FullscreenActivity.java
+++ app/src/main/java/com/rsquared/robert/emotifaceit/FullscreenActivity.java
@@ -2,26 +2,22 @@ package com.rsquared.robert.emotifaceit;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Camera;
+import android.hardware.Camera;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-
-import java.io.IOException;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity implements {
+public class FullscreenActivity extends AppCompatActivity{
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -33,7 +29,8 @@ public class FullscreenActivity extends AppCompatActivity implements {
      * user interaction before hiding the system UI.
      */
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
@@ -98,33 +95,55 @@ public class FullscreenActivity extends AppCompatActivity implements {
 
         setContentView(R.layout.activity_fullscreen);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+        //btn to close the application
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+
+//        mControlsView = findViewById(R.id.fullscreen_content_controls);
+//        mContentView = findViewById(R.id.fullscreen_content);
 
 
         // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
+//        mContentView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                toggle();
+//            }
+//        });
+//    }
 
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
-    }
+//        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+//    }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
+//    @Override
+//    protected void onPostCreate(Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//
+//        // Trigger the initial hide() shortly after the activity has been
+//        // created, to briefly hint to the user that UI controls
+//        // are available.
+//        delayedHide(100);
     }
 
     private void toggle() {
