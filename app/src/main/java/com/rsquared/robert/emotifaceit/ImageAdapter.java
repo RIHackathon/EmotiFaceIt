52025090c3f2d3402321b3d656b67352ffaeb893
robertsrosa15@gmail.com <robertsrosa15@gmail.com>
2016-12-03 19:30:28 -0500

    Added functionality to save bitmap

--- app/src/main/java/com/rsquared/robert/emotifaceit/ImageAdapter.java
+++ app/src/main/java/com/rsquared/robert/emotifaceit/ImageAdapter.java
@@ -3,12 +3,15 @@ package com.rsquared.robert.emotifaceit;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -18,13 +21,18 @@ import java.io.IOException;
 
 public class ImageAdapter extends BaseAdapter {
 
+    private static final String FILE_NAME = "memoji.png";
+
     private Context context;
     private Bitmap bitmap;
-    public ImageAdapter(Context context){
+    private ImageView imageView;
+
+
+    public ImageAdapter(Context context) {
         this.context = context;
     }
 
-    public ImageAdapter(Context context, Bitmap bitmap){
+    public ImageAdapter(Context context, Bitmap bitmap) {
         this.context = context;
         this.bitmap = bitmap;
     }
@@ -46,18 +54,24 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View view, ViewGroup viewGroup) {
-        ImageView imageView = new ImageView(context);
+
+        imageView = new ImageView(context);
 //        imageView.setImageResource(mThumbIds[position]);
+        int imageCount = loadImageFromStorage(getFilePath());
+        saveToInternalStorage(bitmap, imageCount);
         imageView.setImageBitmap(bitmap);
+
         return imageView;
     }
 
-    private String saveToInternalStorage(Bitmap bitmapImage){
+    private String saveToInternalStorage(Bitmap bitmapImage, int imageCount) {
         ContextWrapper cw = new ContextWrapper(context.getApplicationContext());
         // path to /data/data/yourapp/app_data/imageDir
-        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+
+        File directory = cw.getDir(FILE_NAME + imageCount, Context.MODE_PRIVATE);
+
         // Create imageDir
-        File mypath=new File(directory,"profile.jpg");
+        File mypath = new File(directory, "emoti1.jpg");
 
         FileOutputStream fos = null;
         try {
@@ -75,23 +89,38 @@ public class ImageAdapter extends BaseAdapter {
         }
         return directory.getAbsolutePath();
     }
-/*    private Integer[] mThumbIds ={
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk,
-            R.drawable.ic_bus_stop_big_medium_small_turk
 
-    };*/
+    private int loadImageFromStorage(String filePath) {
+
+        int count = 0;
+        boolean hasBitmap = true;
+        while (hasBitmap) {
+
+            try {
+                File file =new File(filePath, FILE_NAME + count);
+                FileInputStream fileInputStream = new FileInputStream(file);
+                if(fileInputStream == null){
+
+
+                }
+                Bitmap bitmap = BitmapFactory.decodeStream(fileInputStream);
+                imageView.setImageBitmap(bitmap);
+                count++;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return count;
+            }
+        }
+        return count;
+    }
+
+    private String getFilePath(){
+        ContextWrapper cw = new ContextWrapper(context.getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir(FILE_NAME, Context.MODE_PRIVATE);
+
+        String mypath = directory.getAbsolutePath();
+
+        return mypath;
+    }
 }
